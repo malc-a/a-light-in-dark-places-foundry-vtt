4d408d5bd12e72fd88186e49d3660457ad37dd0e
Malc Arnold <malc@limpid.org>
2023-07-29 21:51:10 +0100

    Added methods to actors to allow updating pools and injuries
    - Intended to support Token Action HUD. Also added some translation strings

--- system/documents/actor.mjs
+++ system/documents/actor.mjs
@@ -120,40 +120,87 @@ export class ThoseWhoWanderActor extends Actor {
     }
 
     /**
+     * Update the actions for this actor.
+     */
+    updateActions(actions) {
+        // First sanity check the number of actions
+        actions = ((actions ?? 0) >= 1) ? (actions ?? 0) : 1;
+
+        // Now see if we have any combatants to render
+        game.combats.forEach((combat) => {
+            combat.combatants.filter((c) => c.actor === this).map((c) => {
+                c.update({ 'initiative': actions })
+            });
+        });
+
+        // Now update the actor itself
+        return this.update({ 'system.actions': actions });
+    }
+
+    /**
+     * Update the value of a specified pool for this actor.
+     */
+    updatePool(resistance, value) {
+        // If the actor is a minion then there are no pools to update
+        if (this.type === 'minion') {
+            return;
+        }
+
+        // First sanity check the value
+        value = ((value ?? 0) >= 0) ? (value ?? 0) : 0;
+        value = (value > this.getPoolMax(resistance)) ? this.getPoolMax(resistance) : value;
+
+        // Add the required change for this resistance to the object
+        let changes = {};
+        changes['system.resistances.' + resistance + '.pool'] = value;
+        return this.update(changes);
+    }
+
+    /**
+     * Update the value of specified injuries for this actor.
+     */
+    updateInjuries(resistance, injuries) {
+        // If the actor is a menace then there are no injuries to update
+        if (this.type === 'menace') {
+            return;
+        }
+
+        // First sanity check the injuries
+        injuries = ((injuries ?? 0) >= 0) ? (injuries ?? 0) : 0;
+        if (this.type === 'minion') {
+            injuries = (injuries > this.system.dice) ? this.system.dice : injuries;
+        }
+
+        // Default the changes we're making to the actor
+        let changes = {};
+
+        // Handle injuries to minions, which work a little differently
+        if (this.type === 'minion' && resistance === 'minion') {
+            changes['system.injuries'] = injuries;
+        } else if (this.type !== 'minion' && resistance !== 'minion') {
+            changes['system.resistances.' + resistance + '.injuries'] = injuries;
+        }
+
+        // Update the actor with the required change
+        return this.update(changes);
+    }
+
+    /**
      * Restore all pools for this actor to their maximum value
      */
     refreshPools() {
-	// If the actor is a minion then there are no pools to refresh
-	if (this.type === 'minion') {
-	    return;
-	}
+        // If the actor is a minion then there are no pools to refresh
+        if (this.type === 'minion') {
+            return;
+        }
 
         // Build the update to apply
         let changes = {};
 
         // Loop over each resistance and add the update to the map
-        for (let [r, v] of Object.entries(this.system.resistances)) {
-            // Default the pool to the number of dice in the resistance
-            let dice = v.dice
-
-            // Get the label of the pool to check for bonuses
-            let resistance = game.i18n.localize(CONFIG.THOSEWHOWANDER.pools[r]);
-
-            // Calculate the bonus from talents, features, attacks, gear and weapons
-            for (let i of this.items) {
-                if (["talent","feature","attack"].includes(i.type)
-                    || ["gear","weapon"].includes(i.type) && i.system.equipped) {
-                    let bs = i.system.bonus;
-                    let re = new RegExp(`(^|,)\\s*${resistance}\\s+([+-]\\d+)\\s*($|,)`);
-                    let m = bs.match(re);
-                    if (m && m[2] && m[2] != 0) {
-                        dice += parseInt(m[2]) ?? 0;
-                    }
-                }
-            }
-
+        for (let [r, _] of Object.entries(this.system.resistances)) {
             // Add the required change for this resistance to the object
-            changes['system.resistances.' + r + '.pool'] = dice;
+            changes['system.resistances.' + r + '.pool'] = this.getPoolMax(r);
         }
 
         // Set up the data for the chat message
@@ -166,20 +213,34 @@ export class ThoseWhoWanderActor extends Actor {
     }
 
     /**
-     * Update the actions for this actor.
+     * Get the maximum value of a pool (specified by resistance)
      */
-    updateActions(actions) {
-        // First sanity check the number of actions
-        actions = ((actions ?? 0) >= 0) ? (actions ?? 0) : 0;
+    getPoolMax(resistance) {
+        // If the actor is a minion then there are no pools to refresh
+        if (this.type === 'minion') {
+            return 0;
+        }
 
-        // Now see if we have any combatants to render
-        game.combats.forEach((combat) => {
-            combat.combatants.filter((c) => c.actor === this).map((c) => {
-                c.update({ 'initiative': actions })
-            });
-        });
+        // Default the maximum to the number of dice in the resistance
+        let dice = this.system.resistances[resistance].dice ?? 0;
 
-        // Now update the actor itself
-        return this.update({ 'system.actions': actions });
+        // Get the label of the pool to check for bonuses
+        let pool = game.i18n.localize(CONFIG.THOSEWHOWANDER.pools[resistance]);
+
+        // Calculate the bonus from talents, features, attacks, gear and weapons
+        for (let i of this.items) {
+            if (["talent","feature","attack"].includes(i.type)
+                || ["gear","weapon"].includes(i.type) && i.system.equipped) {
+                let bs = i.system.bonus;
+                let re = new RegExp(`(^|,)\\s*${pool}\\s+([+-]\\d+)\\s*($|,)`);
+                let m = bs.match(re);
+                if (m && m[2] && m[2] != 0) {
+                    dice += parseInt(m[2]) ?? 0;
+                }
+            }
+        }
+
+        // And return the calculated maximum
+        return dice;
     }
 }
