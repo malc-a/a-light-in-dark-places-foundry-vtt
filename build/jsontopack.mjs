c0fccf38731ce8f37810815ad026d04fbc556080
Malc Arnold <malc@limpid.org>
2023-08-16 22:01:13 +0100

    Added and used folder handling to the jsontopack and packtojson tools
    - Also added name cleanup to packtojson to avoid problematic filenames
    - Moved gear entries into separate folders by type for ease of access
    - Moved spells into separate folders by school for the same reason
    - Rebuilt all the files in packs with packtojson for consistent formatting
    - Workflow for compendium development no longer requires Foundry v10

--- build/jsontopack.mjs
+++ build/jsontopack.mjs
@@ -11,7 +11,7 @@ async function main() {
         throw new Error('Usage: jsontopack.js type <output dir> <input dir ...>');
     }
     for (let i = 4; i < process.argv.length; i++) {
-        packCompendium(process.argv[2], process.argv[3], process.argv[i]);
+        await packCompendium(process.argv[2], process.argv[i], process.argv[3]);
     }
 }
 
@@ -21,66 +21,116 @@ async function main() {
  * @param {string} output The directory to write to
  * @param {string} input  The directory to read files from
  */
-async function packCompendium(type, output, input) {
+async function packCompendium(type, input, output) {
     // Get the full path to the output DB
-    output += '/' + path.basename(input);
+    output = path.resolve(output, path.basename(input));
 
     // If the output DB exists then clear it
     if (existsSync(output)) { await fs.rm(output, { recursive: true }); }
 
     // Create the DB and start a transaction
-    console.log('Creating ' + output);
     const db = new ClassicLevel(output, { valueEncoding: 'json'});
     const batch = db.batch();
 
-    // Get the files that should be DB records
-    const files = await fs.readdir(path.resolve(input));
-
-    // Read each file in a subdirectory and write it to the DB
-    for (const f of files) {
-        // Read the file and convert it to JSON
-        const file = path.resolve(input, f);
-        const json = JSON.parse(await fs.readFile(file, 'utf-8'));
-
-        // Build the key for this DB record
-        const key = json._key || '!' + type + '!' + json._id;
-
-        // Handle actor items, which v11 splits into separate records
-        let actorItems = [];
-        if (type === 'actors' && json.items.length > 0) {
-            // Update the actor record to link to the items
-            actorItems = json.items;
-            json.items = json.items.map((i) => i._id);
-        }
-
-        // Handle journal pages, which v11 splits into separate records
-        let journalPages = [];
-        if (type === 'journal' && json.pages.length > 0) {
-            // Update the journal record to link to the pages
-            journalPages = json.pages;
-            json.pages = json.pages.map((p) => p._id);
-        }
-
-        // Put the (possibly-updated) record
-        batch.put(key, json);
-
-        // Now add a record for each item for actors
-        for (let i of actorItems) {
-            const key = i.key || '!' + 'actors.items' + '!' + json._id + '.' + i._id;
-            batch.put(key, i);
-        }
-
-        // Or add a record for each page for journals
-        for (let p of journalPages) {
-            const key = p.key || '!' + 'journal.pages' + '!' + json._id + '.' + p._id;
-            batch.put(key, p);
-        }
-    }
+    // Create DB records for each folder or file in the input
+    await packFolders(batch, input);
+    await packFiles(batch, type, input);
 
     // Commit and close the DB
     await batch.write();
     await db.close();
 
     // Confirm success
-    console.log('Built ' + output);
+    await console.log('Built ' + output);
+}
+
+/**
+ * Add any folders found in files to the batch
+ * @param {object} batch The LevelDB batch we're writing
+ * @param {string} dir   The directory we're processing
+ */
+async function packFolders(batch, dir) {
+    // Get the entries in the directory
+    const entries = await fs.readdir(path.resolve(dir));
+
+    // Now process each subdirectory in the entries
+    for (const e of entries) {
+        // We'll need the full path to the entry
+        let entry = path.resolve(dir, e);
+        let stat = await fs.lstat(entry);
+
+        // We process directories that don't begin with '.'
+        if (!e.startsWith('.') && stat.isDirectory()) {
+            // Read the folder details and convert them to JSON
+            const file = path.resolve(entry, '_folder.json');
+            const json = JSON.parse(await fs.readFile(file, 'utf-8'));
+
+            // Build the key and create the DB record
+            const key = '!folders!' + json._id;
+            batch.put(key, json);
+
+            // Recurse to find any subdirectories
+            await packFolders(batch, entry);
+        }
+    }
+}
+
+/**
+ * Add any JSON files found in files to the batch
+ * @param {object} batch The LevelDB batch we're writing
+ * @param {string} type  The type of document to be packed
+ * @param {string} dir   The directory we're processing
+ */
+async function packFiles(batch, type, dir) {
+    // Get the entries in the directory
+    const entries = await fs.readdir(path.resolve(dir));
+
+    // Now process each file in the entries
+    for (const e of entries) {
+        // We'll need the full path to the entry
+        let entry = path.resolve(dir, e);
+        let stat = await fs.lstat(entry);
+
+        // We don't process directories but should recurse into them
+        if (!e.startsWith('.') && stat.isDirectory()) {
+            await packFiles(batch, type, entry);
+        } else if (!e.startsWith('.') && !e.startsWith('_') && e.endsWith('.json')) {
+            // Read the file and convert it to JSON
+            const json = JSON.parse(await fs.readFile(entry, 'utf-8'));
+
+            // Build the key for this DB record
+            const key = '!' + type + '!' + json._id;
+
+            // Handle actor items, which v11 splits into separate records
+            let actorItems = [];
+            if (type === 'actors' && json.items.length > 0) {
+                // Update the actor record to link to the items
+                actorItems = json.items;
+                json.items = json.items.map((i) => i._id);
+            }
+
+            // Handle journal pages, which v11 splits into separate records
+            let journalPages = [];
+            if (type === 'journal' && json.pages.length > 0) {
+                // Update the journal record to link to the pages
+                journalPages = json.pages;
+                json.pages = json.pages.map((p) => p._id);
+            }
+
+            // Put the (possibly-updated) record
+            batch.put(key, json);
+
+            // Now add a record for each item for actors
+            for (let i of actorItems) {
+                const key = i.key || '!' + 'actors.items' + '!' + json._id + '.' + i._id;
+                batch.put(key, i);
+            }
+
+            // Or add a record for each page for journals
+            for (let p of journalPages) {
+                const key = p.key || '!' + 'journal.pages' + '!' + json._id + '.' + p._id;
+                batch.put(key, p);
+            }
+        }
+    }
 }
--- build/packtojson.mjs
+++ build/packtojson.mjs
@@ -1,46 +1,57 @@
 // Dump every record from a LevelDB DB into a JSON document
 import { ClassicLevel } from 'classic-level';
-import { existsSync, promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 
 main();
 
 async function main() {
-    // We need two arguments - the output and input directories
+    // We need at least two arguments - the output and input directories
     if (process.argv.length < 4) {
         throw new Error('Usage: packtojson.mjs <output dir> <input dir ...>');
     }
     for (let i = 3; i < process.argv.length; i++) {
-        unpackCompendium(process.argv[2], process.argv[i]);
+        unpackCompendium(process.argv[i], process.argv[2]);
     }
 }
 
 /**
  * Create a JSON file from the records in a LevelDB DB
- * @param {string} output The directory write records to
  * @param {string} input  The DB to read records from
+ * @param {string} output The directory write records to
  */
-async function unpackCompendium(output, input) {
+async function unpackCompendium(input, output) {
+    // Get the full path to the output directory
+    output = path.resolve(output, path.basename(input));
+
     // Open the DB we're unpacking
-    const db = new ClassicLevel(input, { valueEncoding: 'json'});
+    const db = new ClassicLevel(path.resolve(input), { valueEncoding: 'json'});
+
+    // We have a bunch of state tracking to do
     var records = [];
+    var folders = [];
+    var directories = {};
     var actorItems = {};
     var journalPages = {};
 
-    // Read each record in the DB and add it to the array
+    // Read each record in the DB and add it to the state
     for await (const [key, value] of db.iterator()) {
         // Add actors, items and journals to the records
         if (key.startsWith('!actors!') || key.startsWith('!items!')
             || key.startsWith('!journal!') || key.startsWith('!adventures!')) {
             records.push(cleanUp(value));
+        } else if (key.startsWith('!folders!')) {
+            // Push this record onto the list of folders and the folder -> directory list
+            folders.push(cleanUp(value));
+            directories[value._id] = getName(output, '', directories, value);
         } else {
             // Store actor item or journal page records to be processed later
-            let m = key.match(/^\!actors\.items\!(\w+)\.(\w+)$/);
+            let m = key.match(/^!actors\.items!(\w+)\.(\w+)$/);
             if (m) {
                 actorItems[m[1]] = actorItems[m[1]] || [];
                 actorItems[m[1]].push(value);
             } else {
-                m = key.match(/^\!journal\.pages\!(\w+)\.(\w+)$/);
+                m = key.match(/^!journal\.pages!(\w+)\.(\w+)$/);
                 if (m) {
                     journalPages[m[1]] = journalPages[m[1]] || [];
                     journalPages[m[1]].push(value);
@@ -76,13 +87,20 @@ async function unpackCompendium(output, input) {
         }
     }
 
+    // Now create the directories to represent the folders
+    for (let f of folders) {
+        // Create the directory, add the definition and confirm success
+        const dir = getName(output, '', directories, f);
+        await fs.mkdir(dir, { recursive: true });
+        fs.writeFile(path.resolve(dir, '_folder.json'), JSON.stringify(f, null, 2) + '\n');
+        console.log('Created ' + dir);
+    }
+
     // Now write each record to the output file
     for (let r of records) {
-        // We use the name of the item as the filename
-        const file = output + '/' + r.name.toLowerCase() + '.json';
+        // Write the record and confirm success
+        const file = getName(output, '.json', directories, r);
         fs.writeFile(file, JSON.stringify(r, null, 2) + '\n');
-
-        // Confirm success
         console.log('Wrote ' + file);
     }
 }
@@ -98,16 +116,38 @@ function cleanUp(record) {
     if (record === null) { return record; }
 
     // Remove any _stats property in the record
-    if ('_stats' in record) { delete record._stats };
+    if ('_stats' in record) { delete record._stats; }
 
     // Then set any flags property to empty
-    if ('flags' in record) { record.flags = {} };
+    if ('flags' in record) { record.flags = {}; }
 
     // Now traverse any objects within the record
     for (let key in record) {
-	if (typeof record[key] === 'object') { record[key] = cleanUp(record[key]) };
+        if (typeof record[key] === 'object') { record[key] = cleanUp(record[key]); }
     }
 
     // Return the updated record
     return record;
+}
+
+/**
+ * Get the file name we want to write a value to. This is based on the
+ * value's name field, but replaces assorted characters that are hard
+ * to process in a git repo. The replacement is very opinionated.
+ * @param {string} output      The base output directory
+ * @param {string} suffix      Any suffix to append to the file name
+ * @param {object} directories Maps folder IDs to directories on disk
+ * @param {object} value       The record we're cleaning up
+ */
+function getName(output, suffix, directories, value) {
+    // Figure out the output directory for the file
+    let dir = output;
+    if (value.folder !== null && value.folder in directories) {
+        dir = directories[value.folder];
+    }
+
+    // Get rid of unwieldy characters in the filename
+    const name = value.name.replace(/\s/g, '-').replace(/[()']/g, '')
+          .replace(/&/g, 'and').toLowerCase();
+    return path.resolve(dir, name + suffix);
 }
